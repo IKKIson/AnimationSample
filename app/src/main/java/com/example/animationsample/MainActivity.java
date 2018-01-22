470db02f9b64124456954387c5002e5e2fcad96a
IkkiSON <shout2517@naver.com>
2018-01-22 17:39:44 +0900

    Android Display Optimalize  - Full HD  - Q(Full)HD  - 7 inch  - 10 inch  - 1920X1080 based

--- app/src/main/java/com/example/animationsample/MainActivity.java
+++ app/src/main/java/com/example/animationsample/MainActivity.java
@@ -1,13 +1,34 @@
 package com.example.animationsample;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.pm.ActivityInfo;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.WindowManager;
+
+//import com.airbnb.lottie.LottieAnimationView;
 
 public class MainActivity extends AppCompatActivity {
+
+    //private LottieAnimationView animationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //상태 바 없애기
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
+        //this.InitLottiView();
+
+
     }
+    // Init & Set Lotti Views in Layout
+    /*
+    public void InitLottiView(){
+        animationView = (LottieAnimationView) findViewById(R.id.animation_view);
+        animationView.setAnimation("hello-world.json");
+        animationView.loop(true);
+        animationView.playAnimation();
+    }
+    */
 }
